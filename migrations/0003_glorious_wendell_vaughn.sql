e51c9f9868a5dec21d727e664db64e425b2c580a
Siddhant Jain <sidjain8000@gmail.com>
2026-07-11 01:02:00 +0530

    initial setup

--- migrations/0003_glorious_wendell_vaughn.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `contact` ADD `deleted_at` integer;--> statement-breakpoint
-ALTER TABLE `payment_method` ADD `deleted_at` integer;
--- src/lib/query-keys.ts
+++
@@ -1,5 +0,0 @@
-export const QUERY_KEYS = {
-    SETTINGS: ["settings"] as const,
-    CYCLES: ["cycles"] as const,
-    ENTRIES: ["entries"] as const,
-};
